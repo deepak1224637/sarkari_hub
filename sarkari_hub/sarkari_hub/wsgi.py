457589462250c6c3b1e2e4a23fb1bf5920754bdb
deepakkumar <deepak752820@gmail.com>
2025-07-24 13:02:36 +0530

    change in manage.py and asgi.py and wasgi.py

--- sarkari_hub/sarkari_hub/wsgi.py
+++ sarkari_hub/sarkari_hub/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarkarihub.sarkarihub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sarkari_hub.settings')
 
 application = get_wsgi_application()
+
